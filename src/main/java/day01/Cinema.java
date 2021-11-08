310672dbbb1a43fe7d427c4838dc504148e8fd1f
Bruzsa László <lacika007@gmail.com>
2021-11-08 15:42:04 +0100

    day01

---
+++ src/main/java/day01/Cinema.java
@@ -0,0 +1,49 @@
+package day01;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Cinema {
+
+    private List<Movie> movies = new ArrayList<>();
+
+    public void addMovie(Movie movie) {
+        movies.add(movie);
+    }
+
+    public List<Movie> findMovieByTime(LocalDateTime time) {
+        List<Movie> list = new ArrayList<>();
+        for (Movie m : movies) {
+            for (LocalDateTime ldt : m.getTimes()) {
+                if (ldt.isEqual(time)) {
+                    list.add(m);
+                    break;
+                }
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+        Cinema cinema = new Cinema();
+
+        cinema.addMovie(new Movie("Titanic", Arrays.asList(
+                LocalDateTime.of(2021,11,8,12,45),
+                LocalDateTime.of(2021,11,8,22,25)
+        )));
+        cinema.addMovie(new Movie("Jurassic Park", Arrays.asList(
+                LocalDateTime.of(2021,11,8,19,45),
+                LocalDateTime.of(2021,11,8,20,25),
+                LocalDateTime.of(2021,11,8,22,25)
+        )));
+
+        System.out.println(cinema.findMovieByTime(LocalDateTime.of(2021,11,8,10,45))); // []
+        System.out.println(cinema.findMovieByTime(LocalDateTime.of(2021,11,8,19,45))); // [Jurassic Park]
+        System.out.println(cinema.findMovieByTime(LocalDateTime.of(2021,11,8,22,25))); // [Titanic, Jurassic Park]
+
+    }
+
+}
